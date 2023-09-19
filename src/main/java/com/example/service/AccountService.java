4bddef48085e70d061124861926151f5b8c55374
Joshua Lee <joshlee1117@yahoo.com>
2023-09-19 22:32:44 +0000

    Implement addAccount() method in AccountService

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,21 @@
 package com.example.service;
 
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountService {
+
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Account addAccount(Account account) {
+        return accountRepository.save(account);
+    }
 }
