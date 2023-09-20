8e5072f9fff9a8798652af16dca7629da5aff228
Joshua Lee <joshlee1117@yahoo.com>
2023-09-20 22:46:12 +0000

    Create Exception controller and custom exceptions

---
+++ src/main/java/com/example/exception/ExceptionAndErrorController.java
@@ -0,0 +1,29 @@
+package com.example.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionAndErrorController {
+
+    @ExceptionHandler(InvalidUsernameException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public @ResponseBody String handleInvalidUsername(InvalidUsernameException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(InvalidPasswordException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public @ResponseBody String handleInvalidPassword(InvalidPasswordException e) {
+        return e.getMessage();
+    }
+    
+    @ExceptionHandler(DuplicateUsernameException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public @ResponseBody String handleDuplicateUsername(DuplicateUsernameException e) {
+        return e.getMessage();
+    }
+}
