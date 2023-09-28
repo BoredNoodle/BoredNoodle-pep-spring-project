668be62c5055c0f15c682281fa68631eb5bedbc0
Joshua Lee <joshlee1117@yahoo.com>
2023-09-28 00:40:34 +0000

    Actual Annotate Exception Handler
    Accidentally staged the wrong file in the previous commit

--- src/main/java/com/example/exception/ExceptionAndErrorController.java
+++ src/main/java/com/example/exception/ExceptionAndErrorController.java
@@ -8,31 +8,55 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionAndErrorController {
-
+    /**
+     * InvalidUsernameException handler.
+     * @param e An InvalidUsernameException object.
+     * @return the error message of the exception object and a status of{@code 400 Bad Request}.
+     */
     @ExceptionHandler(InvalidUsernameException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public @ResponseBody String handleInvalidUsername(InvalidUsernameException e) {
         return e.getMessage();
     }
 
+    /**
+     * InvalidPasswordException handler.
+     * @param e An InvalidPasswordException object.
+     * @return the error message of the exception object and a status of{@code 400 Bad Request}.
+     */
     @ExceptionHandler(InvalidPasswordException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public @ResponseBody String handleInvalidPassword(InvalidPasswordException e) {
         return e.getMessage();
     }
     
+    /**
+     * DuplicateUsernameException handler.
+     * @param e An DuplicateUsernameException object.
+     * @return the error message of the exception object and a status of{@code 409 Conflict}.
+     */
     @ExceptionHandler(DuplicateUsernameException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public @ResponseBody String handleDuplicateUsername(DuplicateUsernameException e) {
         return e.getMessage();
     }
 
+    /**
+     * InvalidLoginException handler.
+     * @param e An InvalidLoginException object.
+     * @return the error message of the exception object and a status of{@code 401 Unauthorized}.
+     */
     @ExceptionHandler(InvalidLoginException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public @ResponseBody String handleInvalidLogin(InvalidLoginException e) {
         return e.getMessage();
     }
 
+    /**
+     * InvalidMessageException handler.
+     * @param e An InvalidMessageException object.
+     * @return the error message of the exception object and a status of{@code 400 Bad Request}.
+     */
     @ExceptionHandler(InvalidMessageException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public @ResponseBody String handleInvalidMessage(InvalidMessageException e) {
