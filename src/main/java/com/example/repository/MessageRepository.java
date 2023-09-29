a4d8cb1542012bb800a4aeec0f8e9e70b96dc729
Joshua Lee <joshlee1117@yahoo.com>
2023-09-29 05:44:55 +0000

    Annotate getUserMessages() repository method

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -9,6 +9,11 @@ import org.springframework.data.repository.query.Param;
 import com.example.entity.Message;
 
 public interface MessageRepository extends JpaRepository<Message, Integer>{
+    /**
+     * Native query that finds all messages by a given account id from the database.
+     * @param account_id an int representing an account id.
+     * @return A List of Message objects by the given account id.
+     */
     @Query(value = "SELECT * FROM message WHERE posted_by = :account_id", nativeQuery = true)
     List<Message> getUserMessages(@Param("account_id") int account_id);
 }
